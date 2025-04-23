b5b45d7523761bf86ee140c964a9db879d88f9c1
sidgaikwad <gaikwadsiddharth039@gmail.com>
2025-04-23 16:00:20 +0530

    changes for the resp libarary in the test_go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module redis-in-go
 
 go 1.23.3
+
+require github.com/tidwall/resp v0.1.1 // indirect
--- proto.go
+++ proto.go
@@ -1,7 +1,15 @@
 package main
 
+import "fmt"
+
 type Command struct{}
 
-func parseCommand(msg []byte) (Command, error) {
+func parseCommand(msg string) (Command, error) {
+	t := msg[0]
+	fmt.Println(t)
+	switch t {
+	case '*':
+		fmt.Println(msg[1:])
+	}
 	return Command{}, nil
 }
